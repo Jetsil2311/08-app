8be6024fdfd2934cfc258c62dcacbc33a507e1f7
edreicruz <edrei.cruz@newral.com>
2023-03-03 15:09:22 -0600

    Initial commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import {Route, Router} from "react-router-dom";
+
+export const AppRouterJsx = () => {
+    return (
+            <Router history={}>
+                {/* Login y Register */}
+                <Route/>
+
+                {/* Journal App */}
+                <Route/>
+            </Router>
+    )
+}
